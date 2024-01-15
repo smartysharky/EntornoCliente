bd8e2654da1ac19102eb64a4ef66257a665a4375
smartysharky <anapallaresrod@gmail.com>
2024-01-15 13:21:58 +0100

    web Worker ejemplo

--- 1pruebas/webWorkers/ejemploTrabajador.js
+++ 1pruebas/webWorkers/ejemploTrabajador.js
@@ -2,15 +2,16 @@ var anterior = 0;
 var actual = 1;
 var secuencia = "";
 function temporizador(){
-    aux = anterior + actual;
-    anterior = actual;
-    actual = aux;
-
+    
     if(!anterior){
         secuencia+=" "+actual;
     }else{
         secuencia+=" "+actual;
     }
+
+    aux = anterior + actual;
+    anterior = actual;
+    actual = aux;
     postMessage(secuencia);
     
     setTimeout("temporizador()" , 500);
